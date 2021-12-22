03700fa027ab621bd0936048d297d3f7e9640bc6
Pradnyesh Gore <gore.pradnyesh@gmail.com>
2021-12-22 00:19:30 -0800

    Adding library link step for boost
    library dir search and link libs search

--- Stage2/src/main.cpp
+++ Stage2/src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main()
+#include "boost/program_options.hpp"
+
+
+namespace
 {
-    std::cout << "Stage2 Hello World ! \n";
+	void on_age(int age)
+	{
+	  std::cout << "On age: " << age << '\n';
+	}
+}
+
+int main(int argc, const char *argv[])
+{
+	boost::program_options::options_description desc{"Options"};
+	desc.add_options()
+	  ("help,h", "Help screen")
+	  ("pi", boost::program_options::value<float>()->default_value(3.14f), "Pi")
+	  ("age", boost::program_options::value<int>()->notifier(on_age), "Age");
+
+	boost::program_options::variables_map vm;
+	boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+	notify(vm);
+	
+	if (vm.count("help"))
+	{
+		  std::cout << desc << '\n';
+	}
     return 0;
 }
